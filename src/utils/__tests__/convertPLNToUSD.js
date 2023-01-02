0d577050412e94d899a2125e0ed363467ca167ec
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-01-02 11:37:24 +0100

    Add test @PLN2USD - return Nreturn NaN when input is empty

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -1,4 +1,4 @@
-import { convertPLNToUSD } from '../convertPLNtoUSD';
+import { convertPLNToUSD } from './../convertPLNtoUSD';
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
@@ -6,10 +6,19 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(2)).toBe('$0.57');
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
-  })
+  });
   it('should return NaN when input is text', () => {
+    function ConvertPLNtoUSD(arg) {
+      if (typeof arg === 'string') {
+        return NaN;
+      } else {      
     expect(convertPLNToUSD('7')).toBeNaN();
     expect(convertPLNToUSD('xyz')).toBeNaN();
     expect(convertPLNToUSD('-11')).toBeNaN();
-  })
+  }}});
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  
+  
 });

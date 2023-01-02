5e6895e02ed2a6b3630a918522c8bb7b9aa55275
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-01-02 12:45:21 +0100

    Add test @PLN2USD - return Nreturn /usr/bin/bash.00 when input is a number but it is less than zero

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -19,13 +19,16 @@ describe('ConvertPLNtoUSD', () => {
   it('should return NaN when input is empty', () => {
     expect(convertPLNToUSD()).toBeNaN();
   });
-  it('should throw an error when input is neither a text nor a number', () => {
+  it('should return error when input is neither a text nor a number', () => {
     expect(convertPLNToUSD(false)).toBe('Error');
     expect(convertPLNToUSD({})).toBe('Error');
     expect(convertPLNToUSD([])).toBe('Error');
     expect(convertPLNToUSD(null)).toBe('Error');
     expect(convertPLNToUSD(function() {})).toBe('Error');
   });
-  
-  
+  it('should return $0.00 when input is a number but it is less than zero', () => {
+    expect(convertPLNToUSD(-10)).toBe('$0.00');
+    expect(convertPLNToUSD(-10.5)).toBe('$0.00');
+    expect(convertPLNToUSD(-0.500)).toBe('$0.00');
+  });  
 });

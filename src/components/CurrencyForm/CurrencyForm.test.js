f7efc50520fa9f9f6baa6159ffd5164e50530570
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-01-02 20:26:50 +0100

    Upgrade CurrencyForm.test.js & Change CurrencyForrencyForm.js

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,8 +1,37 @@
-import { render } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Component CurrencyForm', () => {
   it('should render without crashing', () => {
     render(<CurrencyForm action={() => {}} />);
   });
+  it('should run action callback with proper data on form submit', () => {
+    const action = jest.fn();
+
+    // render component
+    render(<CurrencyForm action={action} />);
+
+    // find “convert” button
+    const submitButton = screen.getByText('Convert');
+
+    // find fields elems
+    const inputAmount = screen.getByTestId('inputAmount');
+    const selectFrom = screen.getByTestId('selectFrom');
+    const selectTo = screen.getByTestId('selectTo');
+
+     // set test values to fields
+    userEvent.type(inputAmount, '100');
+    userEvent.selectOptions(selectFrom, 'PLN');
+    userEvent.selectOptions(selectTo, 'USD');
+
+
+    // simulate user click on "convert" button
+    userEvent.click(submitButton);
+
+    // check if action callback was called once and with proper argument
+    expect(action).toHaveBeenCalledTimes(1);
+    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
+
+    });
 });
